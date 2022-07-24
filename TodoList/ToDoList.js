6374062cb4778376872e1b8120dd6bd9e1384b38
buhyun <hwangbh8@naver.com>
2022-07-24 18:59:51 +0900

    onedrive quest submit

--- TodoList/ToDoList.js
+++ TodoList/ToDoList.js
@@ -1,28 +1,30 @@
 var button = document.getElementById('button');
-var input = document.getElementById('input');
+var input = document.getElementById('input');  
+// input 수정
 var list = document.getElementById('list');
 var cnt = 0;  
 
 button.addEventListener('click', clickButton);
 
 function clickButton() {
-    var temp = document.createElement('li');
-    temp.setAttribute("id", "li"+cnt);
-    temp.innerHTML = input.value;
-    temp.innerHTML += "<button id=delete style='float: center;' type='button' onclick='remove("+cnt+")'>삭제</button>";
-    list.appendChild(temp);
+    var todoelement = document.createElement('li');
+    todoelement.setAttribute("id", "li"+cnt);
+    todoelement.innerHTML = input.value;
+    todoelement.innerHTML += "<button type='button' onclick='removetodolist("+cnt+")'>삭제</button>";
+    list.appendChild(todoelement);
     cnt++;
 
-    temp.addEventListener('click', function(){   
-        temp.style.textDecoration = "line-through";
+    todoelement.addEventListener('click', function(){   
+        todoelement.style.textDecoration = "line-through";
     })
-    temp.addEventListener('dblclick', function(){
-        list.removeChild(temp);
+    todoelement.addEventListener('dblclick', function(){
+        list.removeChild(todoelement);
     })
 }
 
-function remove(cnt) {
-    var li = document.getElementById('li'+cnt);
-    list.removeChild(li);
+function removetodolist(todoId) {
+    var todoList = document.getElementById('li'+cnt);
+    list.removeChild(todoList);
 }
 
+// view  공부
